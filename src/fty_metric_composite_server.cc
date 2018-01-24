93d958dedefaa1a07891dadd9731995387033dc0
bstepankova <bstepankova@volny.cz>
2018-01-24 16:00:19 +0100

    Problem: time stamp is zero in messages
    Solution:  fixed
    
    Signed-off-by: Barbora Stepankova <BarboraStepankova@eaton.com>

--- src/fty_metric_composite_server.cc
+++ src/fty_metric_composite_server.cc
@@ -1,21 +1,21 @@
 /*  =========================================================================
     fty_metric_composite_server - Composite metrics server
 
-    Copyright (C) 2014 - 2017 Eaton                                        
-                                                                           
-    This program is free software; you can redistribute it and/or modify   
-    it under the terms of the GNU General Public License as published by   
-    the Free Software Foundation; either version 2 of the License, or      
-    (at your option) any later version.                                    
-                                                                           
-    This program is distributed in the hope that it will be useful,        
-    but WITHOUT ANY WARRANTY; without even the implied warranty of         
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          
-    GNU General Public License for more details.                           
-                                                                           
+    Copyright (C) 2014 - 2017 Eaton
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
     You should have received a copy of the GNU General Public License along
     with this program; if not, write to the Free Software Foundation, Inc.,
-    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.            
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
     =========================================================================
 */
 /*
@@ -246,6 +246,7 @@ fty_metric_composite_server (zsock_t *pipe, void* args) {
             fty_proto_set_value(n_met, "%.2f", lua_tonumber(L, -2));
             fty_proto_set_unit(n_met,  "%s", lua_tostring(L, -1));
             fty_proto_set_ttl(n_met,  TTL);
+            fty_proto_set_time(n_met, std::time (NULL));
             zmsg_t* z_met = fty_proto_encode(&n_met);
             int rv = mlm_client_send(client, lua_tostring(L, -3), &z_met);
             if (rv != 0) {
